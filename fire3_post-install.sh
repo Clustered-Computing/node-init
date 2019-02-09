45fa10197830284d3e2d9be4132fa3b14bbc07cf
Drewry Pope <drewrypope@gmail.com>
2019-02-08 20:01:39 -0600

    wget resolv.conf

--- fire3_post-install.sh
+++ fire3_post-install.sh
@@ -1,1 +1,3 @@
-echo "Hello World"
+echo "Hello World" > ~/HelloWorld.md
+wget -q https://raw.githubusercontent.com/Clustered-Computing/node-init/master/resolv.conf > /etc/resolv.conf
+ping n-cluster > ~/ping.pleasework
