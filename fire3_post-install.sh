8e78b7f8c1dfeee07510ecca4245b086ce51f755
Drewry Pope <drewrypope@gmail.com>
2019-02-09 02:35:30 -0600

    gotta stop pinging eventually..

--- fire3_post-install.sh
+++ fire3_post-install.sh
@@ -1,3 +1,3 @@
 echo "Hello World" > ~/HelloWorld.md
 curl https://raw.githubusercontent.com/Clustered-Computing/node-init/master/resolv.conf -o /etc/resolv.conf
-ping n-cluster > ~/ping.pleasework
+ping -c 5 n-cluster > ~/ping.pleasework
