73a1845896c41abf1eae31acbd230e18d364cafa
Drewry Pope <drewrypope@gmail.com>
2019-02-09 02:01:36 -0600

    Update fire3_post-install.sh

--- fire3_post-install.sh
+++ fire3_post-install.sh
@@ -1,3 +1,3 @@
 echo "Hello World" > ~/HelloWorld.md
-curl -K https://raw.githubusercontent.com/Clustered-Computing/node-init/master/resolv.conf -o /etc/resolv.conf
+curl https://raw.githubusercontent.com/Clustered-Computing/node-init/master/resolv.conf > /etc/resolv.conf
 ping n-cluster > ~/ping.pleasework
