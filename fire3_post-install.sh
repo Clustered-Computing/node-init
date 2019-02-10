3a935f6d8ba5b657dd627553c8066cd333a4d68e
Drewry Pope <drewrypope@gmail.com>
2019-02-09 22:38:57 -0600

    Update fire3_post-install.sh

--- fire3_post-install.sh
+++ fire3_post-install.sh
@@ -1,5 +1,4 @@
 echo "Hello World " $(date) > ~/HelloWorld.md
 curl https://raw.githubusercontent.com/Clustered-Computing/node-init/master/resolv.conf -o /etc/resolv.conf
-/DietPi/dietpi/func/change_hostname $(echo "f3-"$(ip -4 addr show eth0 | grep -oP 'inet \K.[\d.]+' | grep -oP '\d{1,3}$')"-cluster")
 ping -c 5 n-cluster > ~/ping.pleasework
 reboot
